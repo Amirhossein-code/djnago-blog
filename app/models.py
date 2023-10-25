9ca3e5b8e001c18f6016eb96295e1d0468abfc0a
amirhossein Edadi <edadiamirhossein@gmail.com>
2023-10-25 18:02:45 +0330

    Author and post models created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
