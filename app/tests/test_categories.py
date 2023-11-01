2be57aaf40667d09cc89eb2723bdd56c885ee392
amirhossein Edadi <edadiamirhossein@gmail.com>
2023-11-01 19:05:51 +0330

    more tests implemented

--- app/tests/test_categories.py
+++ app/tests/test_categories.py
@@ -16,3 +16,22 @@ class TestCreateCategory:
     def test_if_user_is_anonymus_returns_401(self, create_category):
         response = create_category({"title": "a"})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_data_is_valid_returns_201(self, authenticate, create_category):
+        authenticate()
+        response = create_category({"title": "a"})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["id"] > 0
+
+    def test_if_user_is_authenticated_can_creat_category_returns_201(
+        self, authenticate, create_category
+    ):
+        authenticate()
+        response = create_category({"title": "a"})
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_category):
+        authenticate(is_staff=True)
+        response = create_category({"title": ""})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data["title"] is not None
