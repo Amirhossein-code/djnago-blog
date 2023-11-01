84223241191aa6d82dc28e83f91e8858f623abce
amirhossein Edadi <edadiamirhossein@gmail.com>
2023-11-01 16:16:50 +0330

    Django debug toolbar implemented

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
 ]
