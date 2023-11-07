0e0efc1b3ebcb66959cfebb57897637f2c0dae7e
amirhossein Edadi <edadiamirhossein@gmail.com>
2023-11-07 08:43:57 +0330

    test post updated

--- app/tests/test_posts.py
+++ app/tests/test_posts.py
@@ -3,6 +3,10 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from model_bakery import baker
 from app.models import Author, Post
+from django.conf import settings
+from django.contrib.auth import get_user_model
+import requests
+from rest_framework_simplejwt.tokens import AccessToken
 
 
 @pytest.fixture
@@ -14,12 +18,38 @@ def create_post(api_client):
 
 
 @pytest.fixture
+def authenticate_user():
+    def _authenticate():
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="testuser", password="testShouldBepassword", email="test@email.com"
+        )
+
+        # Generate JWT token using Djoser
+        response = requests.post(
+            "http://127.0.0.1:8000/auth/jwt/create",
+            json={"username": "testuser", "password": "testShouldBepassword"},
+        )
+
+        if response.status_code == 200:
+            token = response.json().get("access")
+        else:
+            raise ValueError("Failed to generate JWT token")
+
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
+        return user
+
+    return _authenticate
+
+
+@pytest.fixture
 def post_data():
-    post = baker.prepare(Post)
     return {
-        "title": post.title,
-        "content": post.content,
-        "category": post.category,
+        "title": "Test Post",
+        "content": "This is the content of the test post.",
+        "category": "Test Category",
     }
 
 
@@ -29,42 +59,17 @@ class TestCreatePost:
         response = create_post(post_data)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    # def test_if_user_is_authenticated_can_create_post_returns_201(
-    #     self, authenticate, create_post
-    # ):
-    #     authenticate()
-    #     post = baker.make(Post)
-    #     data = {
-    #         "title": post.title,
-    #         "content": post.content,
-    #         "category": post.category,
-    #     }
-    #     response = create_post(data)
-    #     assert response.status_code == status.HTTP_201_CREATED
-
-    #     author = baker.make(Author)
-
-    #     # Get the associated user instance from the Author instance
-    #     user = author.user
-
-    #     # Authenticate the user using the authenticated_user fixture
-    #     authenticated_user(user)
-
-    #     post_data = {
-    #         "title": "Test Post",
-    #         "content": "This is a test post",
-    #         "category": "tech",
-    #     }
-    #     response = create_post(post_data)
-    #     assert response.status_code == status.HTTP_201_CREATED
-    #     assert response.data["id"] is not None
-
-    #     assert response.data["title"] == post_data["title"]
-    #     assert response.data["content"] == post_data["content"]
-    #     assert response.data["category"] == post_data["category"]
-
-    # def test_if_data_is_invalid_returns_400(self, authenticate, create_post):
-    #     authenticate()
-    #     response = create_post({"title": "", "content": "Lorem ipsum"})
-    #     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    #     assert response.data["title"] is not None
+    @pytest.mark.skip
+    def test_if_user_is_authenticated_can_create_post_returns_201(
+        self, authenticate_user, create_post
+    ):
+        user = authenticate_user()
+        author_id = user.author.id
+        post_data = {
+            "title": "THis is a test from pytest",
+            "content": "Busginjsdarhjdiprhsaprhb",
+            "category": "1",
+            "author": author_id,
+        }
+        response = create_post(post_data)
+        assert response.status_code == status.HTTP_201_CREATED
