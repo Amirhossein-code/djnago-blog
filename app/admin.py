5c6ed16c07e939908bb3bbfb7e426d50e79e18ee
amirhossein Edadi <edadiamirhossein@gmail.com>
2023-11-16 21:02:21 +0330

    Author admin improve

--- app/admin.py
+++ app/admin.py
@@ -8,48 +8,73 @@ class AuthorAdmin(admin.ModelAdmin):
         "first_name",
         "last_name",
         "phone",
+        "birth_date",
         "joined_at",
-        "user",
+        "slug",
+        "tags",  # bugged
     ]
-    list_select_related = ["user"]
+    # prepopulated_fields = {"slug": ["user__first_name", "user__last_name"]}
+
+    search_fields = ["user__first_name", "user__last_name", "phone"]
+    list_filter = ["joined_at"]
+    readonly_fields = ["joined_at"]
+
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "user",
+                    "phone",
+                    "birth_date",
+                    "bio",
+                    "profile_image",
+                    "website_url",
+                    "social_media_url",
+                    "tags",
+                ]
+            },
+        ),
+        (
+            "Date Information",
+            {
+                "fields": ["joined_at"],
+                "classes": ["collapse"],
+            },
+        ),
+    ]
 
 
-# @admin.register(Post)
-# class PostAdmin(admin.ModelAdmin):
-#     autocomplete_fields = ["category"]
-#     prepopulated_fields = {"slug": ["title"]}
+# @admin.register(Author)
+# class AuthorAdmin(admin.ModelAdmin):
 #     list_display = [
-#         "author",
-#         "id",
-#         "title",
-#         "content",
-#         "category",
-#         "posted_at",
-#         "last_updated",
+#         "first_name",
+#         "last_name",
+#         "phone",
+#         "joined_at",
+#         "user",
 #     ]
-#     list_filter = ["category", "last_updated"]
-#     list_select_related = ["category"]
-#     search_fields = ["title"]
+#     list_select_related = ["user"]
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "category", "author", "posted_at")
-    list_filter = ("category", "author")
-    search_fields = ("title", "content")
-    prepopulated_fields = {"slug": ("title",)}
-    readonly_fields = ("posted_at", "last_updated")
+    list_display = ["title", "category", "author", "posted_at"]
+    list_filter = ["category", "author"]
+    search_fields = ["title", "content"]
+    prepopulated_fields = {"slug": ["title"]}
+    readonly_fields = ["posted_at", "last_updated"]
 
-    fieldsets = (
-        (None, {"fields": ("title", "content", "slug", "category", "author", "tags")}),
+    fieldsets = [
+        (None, {"fields": ["title", "content", "slug", "category", "author", "tags"]}),
         (
             "Date Information",
             {
-                "fields": ("posted_at", "last_updated"),
-                "classes": ("collapse",),
+                "fields": ["posted_at", "last_updated"],
+                "classes": ["collapse"],
             },
         ),
-    )
+    ]
 
     def save_model(self, request, obj, form, change):
         obj.author = request.user.author
